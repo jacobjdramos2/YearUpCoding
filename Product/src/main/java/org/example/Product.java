8a60ee8f2d45bdf87471aee39a7fa598db3a5012
jacobjdramos2 <jandek60@gmail.com>
2025-04-18 11:26:21 -0700

    added

---
+++ Product/src/main/java/org/example/Product.java
@@ -0,0 +1,51 @@
+package org.example;
+
+public class Product {
+    private String name;
+    private double price;
+    int quantity;
+    int id;
+
+    public Product(String name, double price, int quantity, int id) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double addToTotal(Product p, double currentTotal) {
+        return p.getPrice() + currentTotal;
+    }
+}
